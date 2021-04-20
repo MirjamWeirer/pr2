9bb565a6659300be27a22dcc63c316cc44d37ce1
MirjamWeirer <mirjam.weirer@gmx.at>
2021-04-20 10:17:33 +0200

    Wrapper Klasse

---
+++ src/einheit2/WrapperDemo.java
@@ -0,0 +1,16 @@
+package einheit2;
+
+public class WrapperDemo {
+    public static void main(String[] args) {
+        Integer number;
+        Integer number2;
+
+        number = new Integer(222);
+        int i = number.intValue() * 2;
+        System.out.println(i);
+
+        //da autoboxing - primitive type and class type can be exchanged for each other
+        number2 = 222; //new Integer ist not necessary but we still get an instance of an objekt
+        int j = number2 + 7; //should not work because Integer + int but it works due to autoboxing
+    }
+}
